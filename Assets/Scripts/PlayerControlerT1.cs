4fc5f859ceeddf87f9ba2b2a1942cf0cf45fe337
MINFANTEE <maloni_ali@hotmail.com>
2022-09-01 10:01:20 -0500

    JUEGO11-PRUEBA

--- Assets/Scripts/PlayerControlerT1.cs
+++ Assets/Scripts/PlayerControlerT1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerControlerT1 : MonoBehaviour
 {
+    //MALONI
 
     public float velocity =10, jumpForce=5;
     bool puedeSaltar = true;
@@ -16,7 +17,11 @@ public class PlayerControlerT1 : MonoBehaviour
     const int ANIMACION_QUIETO=0;
     const int ANIMACION_CORRER=1;
     const int ANIMACION_ATACAR=2;
-    const int ANIMACION_saltar=4;
+    const int ANIMACION_CAMINAR=3;
+    const int ANIMACION_SALTAR=4;
+
+    private Vector3 lastCheckpointPosition;
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,49 +39,68 @@ public class PlayerControlerT1 : MonoBehaviour
     void Update()
     {
 
-        puedeSaltar = true;
+        //puedeSaltar = true;  //SALTAR MAS DE DOS VECES 
         Debug.Log("Puede saltar"+puedeSaltar.ToString());
 
-        if(Input.GetKey(KeyCode.RightArrow) ){
+        //MOVER DERECHA
+        if(Input.GetKey(KeyCode.RightArrow)&& Input.GetKey("x") ){
 
-            rb.velocity =new Vector2(velocity,rb.velocity.y);
+            rb.velocity =new Vector2(20,rb.velocity.y);
             sr.flipX=false;
             cambiarAnimacion(ANIMACION_CORRER);
         }      
         
+        //MOVER IZQUIERDA
+        else if(Input.GetKey(KeyCode.LeftArrow)&& Input.GetKey("x")){
 
-        else if(Input.GetKey(KeyCode.LeftArrow)){
-
-            rb.velocity =new Vector2(-velocity,rb.velocity.y);
+            rb.velocity =new Vector2(-20,rb.velocity.y);
             sr.flipX=true;
             cambiarAnimacion(ANIMACION_CORRER);
         }
 
+//---------------------------------------------------------------------
+        //MOVER DERECHA
+        else if(Input.GetKey(KeyCode.RightArrow) ){
+
+            rb.velocity =new Vector2(velocity,rb.velocity.y);
+            sr.flipX=false;
+            cambiarAnimacion(ANIMACION_CAMINAR);
+        }      
+        
+        //MOVER IZQUIERDA
+        else if(Input.GetKey(KeyCode.LeftArrow)){
+
+            rb.velocity =new Vector2(-velocity,rb.velocity.y);
+            sr.flipX=true;
+            cambiarAnimacion(ANIMACION_CAMINAR);
+        }
+//---------------------------------------------------------------------
+
+        //SALTAR
         else if(Input.GetKeyUp(KeyCode.Space)&& puedeSaltar){
             rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
             puedeSaltar=false;
+            cambiarAnimacion(ANIMACION_SALTAR);
         }
 
-        //Atacar
+        //ATACAR
         else if(Input.GetKey(KeyCode.Z)){
 
             cambiarAnimacion(ANIMACION_ATACAR);
         }
-        //saltar
-        else if(Input.GetKey(KeyCode.Space)){
-
-            cambiarAnimacion(ANIMACION_saltar);
-        }
-       
+             
 
         else{
-
             rb.velocity = new Vector2(0, rb.velocity.y);
              cambiarAnimacion(ANIMACION_QUIETO);
-
-
         }
 
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        lastCheckpointPosition=transform.position;
+        
     }
 
     void OnCollisionEnter2D(Collision2D other){
@@ -88,6 +112,30 @@ public class PlayerControlerT1 : MonoBehaviour
 
                 Debug.Log("Estas muerto");
             }
+
+            if(other.gameObject.name =="DarkHole"){
+
+            if(lastCheckpointPosition!=null){
+                transform.position=lastCheckpointPosition;
+            }
+
+            }
+//2checkpoint
+//------------------------------------------
+            if(other.gameObject.name =="DarkHole"){
+
+            if(lastCheckpointPosition!=null){
+                transform.position=lastCheckpointPosition;
+            }
+
+            }
+            
+
+//------------------------------------------
+
+
+
+
     }
     void cambiarAnimacion(int animacion){
         animator.SetInteger("Estado",animacion);
